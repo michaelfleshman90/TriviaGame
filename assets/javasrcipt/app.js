51f336fd8b144950d2b40bef06ea02d0edf37336
Michael Fleshman <michaelfleshman90@gmail..com>
2018-07-28 08:41:23 -0400

    question object

---
+++ assets/javasrcipt/app.js
@@ -0,0 +1,41 @@
+game ={
+
+    triviaQuestions = {
+        question1 = {
+            question: "what fictional city is Batman's home?",
+            choice1: "Gotham City",
+            choice2: "Metropolis",
+            choice3: "Central City",
+            choice4: "Themyscira",
+            correctAnswer: choice1,
+        },
+        question2 = {
+            question: "What is Superman's real name?",
+            choice1: "Clark Kent",
+            choice2: "Bobby Hill",
+            choice3: "Kal-El",
+            choice4: "Billy Mays",
+            correctAnswer: choice3,
+        },
+        question3 = {
+            question: "How old is Wonder Woman?",
+            choice1: "250yrs old",
+            choice2: "5000yrs old",
+            choice3: "28yrs old",
+            choice4: "Who knows?",
+            correctAnswer: choice2,
+        },
+        question4 = {
+            
+        } 
+        question5: "What type of dog has breeds called Scottish, Welsh, and Irish?",
+        question6: "Babe Ruth is associated with which sport?",
+        question7: "Who was known as the Maid of Orleans?",
+        question8: "In the film Babe, what type of animal was Babe?",
+    },
+    
+
+
+
+
+}
